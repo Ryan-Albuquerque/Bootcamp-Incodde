e60ba7b17225f27a3f9e49f68aac26380ccd3045
Ryan-Albuquerque <ryan.albuquerque2002@gmail.com>
2020-05-22 17:25:27 -0400

    server node

--- T2/Semana1/routes.js
+++ T2/Semana1/routes.js
@@ -1,15 +1,8 @@
 const express = require("express")
-const path = require("path")
 
 const route = express.Router()
 
-route.get("/desafio", (req,res)=>{
-    res.sendFile(path.join(__dirname + "/desafioJSDOM/index.html"))
-})
+//controllers
 
-
-route.get("/fakeGoogle", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/desafio_fake_google/index.html"))
-})
 
 module.exports = route
--- T2/Semana1/server.js
+++ T2/Semana1/server.js
@@ -3,9 +3,8 @@ const path = require("path")
 
 const app = express();
 
-// app.use(require("./routes"))
+app.use(express.static(path.join(__dirname + "/public")))
 
-app.use('/booking_store', express.static(path.join(__dirname + "/desafioJSDOM/")))
-app.use('/fake_google', express.static(path.join(__dirname + "/desafio_fake_google/")))
+app.use(require("./routes"))
 
 app.listen(3000);
