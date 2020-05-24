f654627dc677746b1181f0266b9bc3a82daf67c9
Ryan-Albuquerque <ryan.albuquerque2002@gmail.com>
2020-05-23 23:17:23 -0400

    controller

--- T2/Semana1_desafio/backend/router.js
+++ T2/Semana1_desafio/backend/router.js
@@ -4,63 +4,15 @@ const express = require("express")
 
 const route = express.Router()
 
-route.get("/", (req, res)=>{
-    return res.status(200).json(db)
-})
+const UserController = require("./controllers/User")
 
-route.post('/test', (req, res) => {
-    let i = 0
-    if(db.length == 0){
-        db.push(req.body) 
-        return res.status(201).send("enviado")
-    }
-    db.forEach(element => {
-        if(element.username === req.body.username){ 
-            i = 1
-            return res.status(409).send("impossivel criar") 
-        }  
-    });
+route.get("/", UserController.index)
 
-    if (i!=1) {
-        db.push(req.body)
-        return res.status(201).send("criado")        
-    }
-});
+route.post('/test', UserController.create);
 
-route.get("/user/:username", (req,res)=>{
-    let i =0
-    db.forEach(element => {
-        if(element.username === req.params.username){
-            i = 1
-            return res.status(200).json(element)
-        }
-    });
+route.get("/user/:username", UserController.filter)
 
-    if(i!=1){
-        return res.status(404).send("nao encontrado")
-    }
-})
-
-route.delete("/user/delete/:username", (req,res)=>{
-    
-    let i = 0
-    db.forEach(element => {
-        if(element.username === req.params.username){
-            pos = db.indexOf(element)
-
-            db.splice(pos, 1)
-
-            i = 1
-            return res.status(200).send("elemento eliminado")
-        }
-    });
-
-    if(i!=1){
-        return res.status(404).send("user nao encontraod")
-    }
-
-
-})
+route.delete("/user/delete/:username", UserController.delete)
 
 module.exports = route
 
